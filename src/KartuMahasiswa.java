718b497811f310130904186e665b274b5f2d910f
Vivaldhy Gabriel Valentino P.Silitonga <2581034@unai.edu>
2026-09-19 20:25:59 +0700

    Menambahkan Data Diri

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -1,11 +1,36 @@
 import java.util.Scanner;
 
 public class KartuMahasiswa {
-    public static void KartuMahasiswa(String args[]) {
+    public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
 
     System.out.print("NIK (16 Digit): ");
     long nikViv = scanner.nextLong();
+
+    System.out.print("Umur: ");
+    int umurViv = scanner.nextInt();
+
+    scanner.nextLine();
+
+    System.out.print("Tinggi Badan: ");
+    double tinggiBadan = scanner.nextDouble();
+
+    scanner.nextLine();
+
+    System.out.print("Golongan Darah: ");
+    char golonganDarah = scanner.next().charAt(0);
+
+    System.out.print("Sudah menikah: ");
+    boolean menikahViv = scanner.nextBoolean();
+
+    scanner.nextLine();
+
+    System.out.print("Nama Lengkap: ");
+    String namaLengkap = scanner.nextLine();
+
+
+
+
 
     }
 }
