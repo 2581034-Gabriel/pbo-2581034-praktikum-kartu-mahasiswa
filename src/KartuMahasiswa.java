e52cf9d5b5449c0a2e272a444d1bd7bca0417a13
Vivaldhy Gabriel Valentino P.Silitonga <2581034@unai.edu>
2026-09-19 20:14:30 +0700

    Menambahkan NIK

---
+++ src/KartuMahasiswa.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class KartuMahasiswa {
+    public static void KartuMahasiswa(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("NIK (16 Digit): ");
+    long nikViv = scanner.nextLong();
+
+    }
+}
